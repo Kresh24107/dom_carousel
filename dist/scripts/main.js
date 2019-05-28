e1d764f1b74767e93f746fe6182e5060a84f1123
Vadim Sirenko <vitalievichkresh@gmail.com>
2019-05-28 22:38:19 +0300

    fixes some bugs

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -2,7 +2,15 @@
 const container = document.querySelector('.container');
 const buttonNext = document.querySelector('.button-next');
 const buttonPrev = document.querySelector('.button-prev');
-const posX = container.scrollLeft;
+
+const countImg = container.children.length;
+const imgWidth = container.children[0].clientWidth;
+const visibleImg = 3;
+const windowWidth = visibleImg * container.children[0].clientWidth;
+const maxRight = countImg * imgWidth - windowWidth;
+let posX = 0;
+
+console.dir(countImg);
 
 buttonNext.disabled = false;
 buttonPrev.disabled = true;
@@ -10,32 +18,32 @@ buttonPrev.disabled = true;
 buttonNext.addEventListener('click', scrollNext);
 buttonPrev.addEventListener('click', scrollPrev);
 
-if (container.scrollLeft > 0 && container.scrollLeft < 910) {
+if (container.scrollLeft && container.scrollLeft < maxRight) {
   buttonPrev.disabled = false;
   buttonNext.disabled = false;
 }
 
 function scrollNext() {
-  const x = container.scrollLeft;
-  container.scrollLeft = x + 390;
-  if (container.scrollLeft === 910) {
+  posX += windowWidth;
+  container.scrollLeft = posX;
+  if (container.scrollLeft === maxRight) {
     buttonPrev.disabled = false;
     buttonNext.disabled = true;
   }
-  if (container.scrollLeft > 0 && container.scrollLeft < 910) {
+  if (container.scrollLeft && container.scrollLeft < maxRight) {
     buttonPrev.disabled = false;
     buttonNext.disabled = false;
   }
 }
 
 function scrollPrev() {
-  const x = container.scrollLeft;
-  container.scrollLeft = x - 390;
+  posX -= windowWidth;
+  container.scrollLeft = posX;
   if (posX === 0) {
     buttonPrev.disabled = true;
     buttonNext.disabled = false;
   }
-  if (container.scrollLeft > 0 && container.scrollLeft < 910) {
+  if (container.scrollLeft && container.scrollLeft < maxRight) {
     buttonPrev.disabled = false;
     buttonNext.disabled = false;
   }
